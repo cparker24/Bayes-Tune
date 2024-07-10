086e69f6687c273c7c1cd64d8bfefdf7003b4f87
Cameron Parker <cameron.parker@tamu.edu>
2024-07-10 16:38:18 -0500

    creating script to build all initial pkl files

---
+++ build_obs.py
@@ -0,0 +1,6 @@
+from src.readers import *
+import ROOT
+import numpy as np
+import pandas as pd
+
+buildObsPkl("/scratch/user/cameron.parker/projects/JETSCAPE/runs/LEPgluonmove","hadrons","hadrons",500)
