8dfc4eed8e5cb1a4019a886357ccb0f266b43f0e
Cameron Parker <cameron.parker@tamu.edu>
2024-07-14 18:36:56 -0500

    making AllData something to import and trim

--- src/data_objects.py
+++ src/data_objects.py
@@ -1,15 +1,19 @@
-from readers import *
+from .readers import *
 
-LEPdir = '/scratch/user/cameron.parker/projects/JETSCAPE/runs/LEPgluonmove/QVir_Analysis/'
+LEPdir = '/data/rjfgroup/rjf01/cameron.parker/runs/LEPdesign/QVir_Analysis/'
 
 AllData = {}
-AllData["Design"] = ReadDesign(LEPdirr+"parameters.txt")
+AllData["Design"] = ReadDesign(LEPdir+"parameters.txt")
 AllData["Observables"] = {"EpEm91": {}}
-AllData["Observables"]["EpEm91"]["charged-xp"] = {"data": ReadData('/scratch/user/cameron.parker/projects/STAT/input/vac-data/Data_ALEPH_EpEm91_charged-xp.dat'),
+AllData["Observables"]["EpEm91"]["charged-xp"] = {"data": ReadData('/data/rjfgroup/rjf01/cameron.parker/builds/STAT/input/vac-data/Data_ALEPH_EpEm91_charged-xp.dat'),
                                                   "predictions": ReadPrediction(LEPdir+'charged-xp.dat')}
-AllData["Observables"]["EpEm91"]["pion-xp"] = {"data": ReadData('/scratch/user/cameron.parker/projects/STAT/input/vac-data/Data_ALEPH_EpEm91_pion-xp.dat'),
+AllData["Observables"]["EpEm91"]["pion-xp"] = {"data": ReadData('/data/rjfgroup/rjf01/cameron.parker/builds/STAT/input/vac-data/Data_ALEPH_EpEm91_pion-xp.dat'),
                                                   "predictions": ReadPrediction(LEPdir+'pion-xp.dat')}
-AllData["Observables"]["EpEm91"]["kaon-xp"] = {"data": ReadData('/scratch/user/cameron.parker/projects/STAT/input/vac-data/Data_ALEPH_EpEm91_kaon-xp.dat'),
+AllData["Observables"]["EpEm91"]["kaon-xp"] = {"data": ReadData('/data/rjfgroup/rjf01/cameron.parker/builds/STAT/input/vac-data/Data_ALEPH_EpEm91_kaon-xp.dat'),
                                                   "predictions": ReadPrediction(LEPdir+'kaon-xp.dat')}
-AllData["Observables"]["EpEm91"]["proton-xp"] = {"data": ReadData('/scratch/user/cameron.parker/projects/STAT/input/vac-data/Data_ALEPH_EpEm91_proton-xp.dat'),
+AllData["Observables"]["EpEm91"]["proton-xp"] = {"data": ReadData('/data/rjfgroup/rjf01/cameron.parker/builds/STAT/input/vac-data/Data_ALEPH_EpEm91_proton-xp.dat'),
                                                   "predictions": ReadPrediction(LEPdir+'proton-xp.dat')}
+# defining emulator paths
+for system in AllData["Observables"]:
+    for obs in AllData["Observables"][system]:
+        AllData["Observables"][system][obs]["emulator"]["file"] = "emulators/"+system+obs+"-emulator.sav"
