699af13480e24eb98547505f4e685c5fe1516ada
Cameron Parker <cameron.parker@tamu.edu>
2024-07-16 12:20:01 -0500

    adding cuts and plot vars

--- src/data_objects.py
+++ src/data_objects.py
@@ -7,16 +7,25 @@ AllData["Design"] = ReadDesign(LEPdir+"parameters.txt")
 AllData["Observables"] = {"EpEm91": {}}
 AllData["Observables"]["EpEm91"]["charged-xp"] = {"data": ReadData('/data/rjfgroup/rjf01/cameron.parker/builds/STAT/input/vac-data/Data_ALEPH_EpEm91_charged-xp.dat'),
                                                   "predictions": ReadPrediction(LEPdir+'charged-xp.dat'),
-                                                  "emulator": {}}
+                                                  "emulator": {},
+                                                  "plotvars": ["$x_p$","$dN/dx_p$","log","log"],
+                                                  "cuts": [np.s_[-2:]]}
 AllData["Observables"]["EpEm91"]["pion-xp"] = {"data": ReadData('/data/rjfgroup/rjf01/cameron.parker/builds/STAT/input/vac-data/Data_ALEPH_EpEm91_pion-xp.dat'),
                                                   "predictions": ReadPrediction(LEPdir+'pion-xp.dat'),
-                                                  "emulator": {}}
+                                                  "emulator": {},
+                                                  "plotvars": ["$x_p$","$dN/dx_p$","log","log"],
+                                                  "cuts": [np.s_[-2:]]}
 AllData["Observables"]["EpEm91"]["kaon-xp"] = {"data": ReadData('/data/rjfgroup/rjf01/cameron.parker/builds/STAT/input/vac-data/Data_ALEPH_EpEm91_kaon-xp.dat'),
                                                   "predictions": ReadPrediction(LEPdir+'kaon-xp.dat'),
-                                                  "emulator": {}}
+                                                  "emulator": {},
+                                                  "plotvars": ["$x_p$","$dN/dx_p$","log","log"],
+                                                  "cuts": [np.s_[-2:]]}
 AllData["Observables"]["EpEm91"]["proton-xp"] = {"data": ReadData('/data/rjfgroup/rjf01/cameron.parker/builds/STAT/input/vac-data/Data_ALEPH_EpEm91_proton-xp.dat'),
                                                   "predictions": ReadPrediction(LEPdir+'proton-xp.dat'),
-                                                  "emulator": {}}
+                                                  "emulator": {},
+                                                  "plotvars": ["$x_p$","$dN/dx_p$","log","log"],
+                                                  "cuts": [np.s_[-2:]]}
+
 # defining emulator paths
 for system in AllData["Observables"]:
     for obs in AllData["Observables"][system]:
