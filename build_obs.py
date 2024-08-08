2d74ae3514dec378208da5ee3f7adbe72b7a5596
Cameron Parker <cameron.parker@tamu.edu>
2024-08-07 19:00:54 -0500

    removing unneeded files

--- build_obs.py
+++
@@ -1,9 +0,0 @@
-from src.readers import *
-import ROOT
-import numpy as np
-import pandas as pd
-
-buildObsPkl("/scratch/user/cameron.parker/projects/JETSCAPE/runs/LEPgluonmove","hadrons","hadrons",500)
-buildObsPkl("/scratch/user/cameron.parker/projects/JETSCAPE/runs/LEPgluonmove","pions","pions",500)
-buildObsPkl("/scratch/user/cameron.parker/projects/JETSCAPE/runs/LEPgluonmove","kaons","kaons",500)
-buildObsPkl("/scratch/user/cameron.parker/projects/JETSCAPE/runs/LEPgluonmove","protons","protons",500)
